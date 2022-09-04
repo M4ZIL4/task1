fd5f237b089768dd50bbf6d246b7821751d1eb59
Danila Shakurov <danilxshakurov@gmail.com>
2022-09-04 16:57:55 +0400

    Решение задачи со звёздочкой

--- task1.java
+++ task1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.TreeMap;
+import java.util.*;
 
 public class task1 {
     public static void main(String[] args) {
@@ -34,19 +34,48 @@ public class task1 {
                 System.out.println("fail: " + conection.getResponseCode() + ", " + conection.getInputStream());
             }
 
-            String simbolsRemoval = sb.toString().replaceAll("\\p{Punct}", "");
-            String removingSpaces = simbolsRemoval.replaceAll("\\s", "");
-            String lowerCase = removingSpaces.toLowerCase();
+            String simbolsRemoval = sb.toString().replaceAll("[^\\p{L}\\p{N}]+", "");
+            String lowerCase = simbolsRemoval.toLowerCase();
 
             TreeMap<Character, Integer> string = new TreeMap<>();
 
             for (int i = 0; i < lowerCase.length(); i++) {
                 Character key = lowerCase.charAt(i);
                 string.put(key, string.getOrDefault(key, 0) + 1);
-
             }
+
             System.out.println("Сколько раз выводится каждый символ: " + string);
 
+            String str = String.valueOf(string);
+
+            String firstLineChange = str.replaceAll("=[0-9]", "");
+            String secondLineChange = firstLineChange.replaceAll("[^\\p{L}\\p{N}]+", "");
+
+            int numberOfUniqueCharacters = 0;
+            for (int i = 0; i < secondLineChange.length(); i++) {
+                numberOfUniqueCharacters++;
+            }
+
+            int sumOfSymbols = 0;
+            for (int i = 0; i < lowerCase.length(); i++) {
+                sumOfSymbols++;
+            }
+
+            double x = sumOfSymbols;
+            double y = numberOfUniqueCharacters;
+            double average = x / y;
+
+            System.out.println("Среднее значение частоты: " + average);
+
+            int rounded = (int)Math.round(average);
+
+            Set<Map.Entry<Character,Integer>> entrySet = string.entrySet();
+            Object desiredObject = rounded;
+            for (Map.Entry<Character,Integer> pair : entrySet) {
+                if (desiredObject.equals(pair.getValue())) {
+                    System.out.println("Символ: " + pair.getKey() + "(" + Character.codePointAt(new char[] {pair.getKey()},0) + ")");
+                }
+            }
         } catch (Throwable cause) {
             cause.printStackTrace();
         } finally {
